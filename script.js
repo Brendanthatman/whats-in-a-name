6658753a7f9e1d20e7cbed1ec0c6248a8d17d695
Brendan Conard <117928315+Brendanthatman@users.noreply.github.com>
2025-02-20 20:29:01 +0000

    button style and cleaned up code

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Return for the function helped in part for searching random number JS and using this article:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function generateNum(){
-    return Math.floor(Math.random() * 11)
+    return Math.floor(Math.random() * 10)
 }
